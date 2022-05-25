356ce758a296874aedd7182a88a607c2c8140e42
romucurrao <romucurrao@gmail.com>
2022-05-25 16:54:15 -0300

    Agregue la suma

--- src/com/company/app/Main.java
+++ src/com/company/app/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int x=1;
+        int z=2;
+        int suma= x+z;
+        System.out.println (suma);
     }
 }
