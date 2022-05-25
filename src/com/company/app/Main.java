5051dddc7acb730ee364466847ef76129e95f56d
Mateo Salgado <salgadomateo001@gmail.com>
2022-05-25 17:04:28 -0300

    fix

--- src/com/company/app/Main.java
+++ src/com/company/app/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int x=1;
+        int z=2;
+        int suma= x+z;
+        System.out.println (suma);
+
         System.out.println ("Estamos listos chicos?  ");
     }
 }
