9c32a1473dd400418a8435455a41d9a66a57d4a2
romucurrao <romucurrao@gmail.com>
2022-05-25 16:51:17 -0300

    Intento de git Cambio linea 7 del main

--- src/com/company/app/Main.java
+++ src/com/company/app/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println ("Estamos listos chicos?");
+        System.out.println ("Estamos listos chicos?  ");
     }
 }
